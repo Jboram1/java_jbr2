406fc80d64780ecacf425ae94deb801b0d9c0e15
KOREAVC <KOREAVC@DESKTOP-0R26I5N>
2023-09-08 18:14:34 +0900

    9월 8일 수업

---
+++ p0908/src/p0908/J0908_12.java
@@ -0,0 +1,14 @@
+package p0908;
+
+public class J0908_12 {
+
+	public static void main(String[] args) {
+		//J0908_11 total 메소드를 호출해서 사용함.
+		System.out.println(J0908_11.total(10,10,9));
+		
+		J0908_11 j = new J0908_11();
+		System.out.println(J0908_11.avg(10,10,9));
+
+	}
+
+}
