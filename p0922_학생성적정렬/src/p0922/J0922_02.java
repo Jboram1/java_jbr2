2c596a505b19026591488ebd816407495217217e
KOREAVC <KOREAVC@DESKTOP-0R26I5N>
2023-09-22 17:56:19 +0900

    9월 22 학생성적정렬

---
+++ p0922_학생성적정렬/src/p0922/J0922_02.java
@@ -0,0 +1,34 @@
+package p0922;
+
+public class J0922_02 {
+
+	public static void main(String[] args) {
+		
+		
+		Week w = Week.FRIDAY;
+		String w2 = "FRIDAY";
+		
+		String weekName = w.name(); //w참조변수 데이터이름 가져옴.
+		System.out.println(weekName);
+		System.out.println(w.name());
+		System.out.println(w2);
+		
+		
+		
+		
+
+//		String[] kind = {"Heart","HEART","heart","hEart"};
+//		for(int i=0;i<kind.length;i++) {
+//			if(kind[i].equals("Heart")) { //한가지로 지정해 주는 걸 Enum. 똑같은 것만 인식.(대소문자구분함)
+//				System.out.println("♥");
+//			}
+//		}//for
+		
+		
+		
+		
+		
+		
+		
+	}//main
+}//class
